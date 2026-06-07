0517f8b61b07903e47bcd2eecd26bbcd4a0ace26
pabloyacante <143475348+pabloyacante@users.noreply.github.com>
2026-06-07 18:20:47 -0300

    CustomMiAnalizadorVisitor.js

--- CustomMiAnalizadorVisitor.js
+++ CustomMiAnalizadorVisitor.js
@@ -7,7 +7,7 @@ export default class CustomMiAnalizadorVisitor extends MiAnalizadorVisitor {
     }
 
     visitInstrucciones(ctx) {
-        // Como tu gramática es recursiva (instrucciones: instruccion | instrucciones instruccion)
+      
         if (ctx.instrucciones()) {
             return this.visit(ctx.instrucciones()) + '\n' + this.visit(ctx.instruccion());
         } else {
@@ -22,33 +22,32 @@ export default class CustomMiAnalizadorVisitor extends MiAnalizadorVisitor {
     visitBucle(ctx) {
         const condicion = ctx.condicion().getText();
         const sentencias = this.visit(ctx.sentencia());
-        // Traducimos el bucle C a un bucle JavaScript (como texto)
+       
         return `while (${condicion}) {\n  ${sentencias}\n}`;
     }
 
     visitSentencia(ctx) {
-        // salida sentencia
+        
         if (ctx.sentencia()) {
             return this.visit(ctx.salida()) + '\n  ' + this.visit(ctx.sentencia());
         } 
-        // salida sola
+       
         else if (ctx.salida()) {
             return this.visit(ctx.salida());
         } 
-        // terminar solo
+       
         else if (ctx.terminar()) {
             return this.visit(ctx.terminar());
         }
     }
 
     visitSalida(ctx) {
-        const cadena = ctx.cadena().getText(); // Obtiene algo como "Hola Mundo"
-        // Traducimos de printf (C) a console.log (JS)
+        const cadena = ctx.cadena().getText(); 
         return `console.log(${cadena});`;
     }
 
     visitTerminar(ctx) {
-        // Traducimos el break
+       
         return `break;`;
     }
 }
